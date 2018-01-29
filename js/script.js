bbfbbf17723076d53df253155b93bfbe709ca2a3
Matt88120 <matt88120@gmail.com>
2018-01-30 00:22:42 +0100

    local game working with styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var table;
+var players = ["O", "X"];
+var lastPlayed;
 
 const buildTable = () => {
   var t = new Array([], [], []);
@@ -35,6 +37,130 @@ const buildHTMLTable = (table) => {
   }
 }
 
+const checkDiagonals = () => {
+  var gameArea = document.getElementById('game');
+  var i = 0;
+  if (table[1][1] === lastPlayed) {
+    if ((table[0][0] === lastPlayed) && (table[2][2] === lastPlayed)) {
+      Array.from(gameArea.getElementsByClassName('line')).map(l => {
+        l.getElementsByClassName('case')[i].className += " success";
+        i++;
+      });
+      return true;
+    } else if ((table[0][2] === lastPlayed) && (table[2][0] === lastPlayed)) {
+      i = 2;
+      Array.from(gameArea.getElementsByClassName('line')).map(l => {
+        l.getElementsByClassName('case')[i].className += " success";
+        i--;
+      });
+      return true;
+    }
+  }
+  return false;
+}
+
+const checkVerticals = () => {
+  var gameArea = document.getElementById('game');
+  var ret = false;
+  var lineIndex;
+  for (var i = 0; i < table.length; i++) {
+    lineIndex = 0;
+    while (lineIndex < table.length) {
+      var ret = (table[lineIndex][i] === lastPlayed);
+      if (!ret) {
+        break;
+      }
+      lineIndex++;
+    }
+    if (ret) {
+      lineIndex = 0;
+      while (lineIndex < table.length) {
+        gameArea.getElementsByClassName('line')[lineIndex].getElementsByClassName('case')[i].className += " success";
+        lineIndex++;
+      }
+      return ret;
+    }
+  }
+}
+
+const checkHorizontals = () => {
+  var gameArea = document.getElementById('game');
+  var ret = false;
+  for (var i = 0; i < table.length; i++) {
+    ret = ((table[i][0] === lastPlayed) && (table[i][1] === lastPlayed) && (table[i][2] === lastPlayed));
+    if (ret) {
+      var line = gameArea.getElementsByClassName('line')[i];
+      for (var j = 0; j < table[i].length; j++) {
+        line.getElementsByClassName('case')[j].className += " success";
+      }
+      return ret;
+    }
+  }
+}
+
+const checkVictory = () => {
+  return (checkDiagonals() || checkHorizontals() || checkVerticals());
+}
+
+const removeListeners = () => {
+  for (var i = 0; i < table.length; i++) {
+    for (var j = 0; j < table[i].length; j++) {
+      var selectedCase = gameArea.getElementsByClassName('line')[i].getElementsByClassName('case')[j];
+      if (typeof selectedCase.attributes.handler !== "undefined") {
+          selectedCase.removeEventListener('click', selectedCase.attributes.handler);
+      }
+    }
+  }
+}
+
+const clickOnCase = (evt, player) => {
+  var targ = evt.target;
+  var lIndex = targ.attributes.lineIndex;
+  var cIndex = targ.attributes.caseIndex;
+  targ.innerHTML = player;
+  targ.className += " " + player + "player clicked";
+  if (lIndex < table.length) {
+    if (cIndex < table[lIndex].length) {
+      table[lIndex][cIndex] = player;
+    }
+  }
+  lastPlayed = player;
+  if (!checkVictory(table)) {
+    if (!tableIsCompleted(table)) {
+      console.log(table);
+      nextPlay(lastPlayed, players);
+    }
+  } else {
+    removeListeners();
+  }
+}
+
+const nextPlay = (last, char) => {
+  if (char.__proto__.constructor.name === "Array") {
+    var nextPlayer;
+    var gameArea = document.getElementById('game');
+    if (typeof last === "undefined") {
+      last = char[1];
+    }
+    nextPlayer = char.find(el => (el !== last));
+    for (var i = 0; i < table.length; i++) {
+      for (var j = 0; j < table[i].length; j++) {
+        var selectedCase = gameArea.getElementsByClassName('line')[i].getElementsByClassName('case')[j];
+        if (typeof selectedCase.attributes.handler !== "undefined") {
+            selectedCase.removeEventListener('click', selectedCase.attributes.handler);
+        }
+        if (typeof table[i][j] === "undefined") {
+          var handler = e => {
+            clickOnCase.apply(this, [e, nextPlayer]);
+          }
+          selectedCase.attributes.handler = handler;
+          selectedCase.addEventListener('click', handler);
+        }
+      }
+    }
+  }
+}
+
 const tableIsCompleted = (table) => {
   var ret = true;
   if ((typeof table !== "undefined") && (typeof table.map !== "undefined")) {
@@ -60,4 +186,4 @@ const tableIsCompleted = (table) => {
 
 table = buildTable();
 buildHTMLTable(table);
-/*console.log(tableIsCompleted(table));*/
+nextPlay(lastPlayed, players);
